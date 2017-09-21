ca12c83524e6b3214087fc9ab1a5aa9c78ff9016
Christophe De Troyer <christophe@call-cc.be>
2017-09-21 11:53:23 +0200

    labeled exps

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,7 +1,7 @@
 module Parser where
 import Text.Parsec.String
 import Data.Char
-import Text.Parsec
+import Text.Parsec hiding (label)
 import Control.Monad
 
 data Register
@@ -15,17 +15,31 @@ data Register
 data Operand
  = Mode0 Register
  | Mode1 Register
- | Const Int
+ | Immed Int
  deriving (Show, Eq, Read)
 
-data Instruction
+data Operator
  = MOV
  | ADD
  deriving (Show, Eq, Read)
 
-data Operation = TwoOp Instruction Operand Operand deriving (Show, Eq, Read)
+type Label = String
 
-type Program = [Operation]
+data Operation
+  = TwoOp Operator Operand Operand
+  deriving (Show, Eq, Read)
+
+data LabeledOperation
+  = Labeled Label Operation
+  deriving (Show, Eq, Read)
+
+data Instruction
+  = Op Operation
+  | LOp LabeledOperation
+  deriving(Show, Eq, Read)
+
+
+type Program = [Instruction]
 
 -- ************************************************************************** --
 
@@ -40,10 +54,6 @@ lexeme p = do
   x <- p
   whitespace
   return x
-
--- Parses a label.
-label :: Parser String
-label = many1 upper
 
 -- Reads a register token
 register :: Parser Register
@@ -61,23 +71,47 @@ mode1 :: Parser Operand
 mode1 = do
   void $ lexeme$  char '('
   r <- lexeme register
-  void $ lexeme$  char ')'
+  void $ lexeme $  char ')'
   return $ Mode1 r
 
 -- Parsers a number.
 number :: Parser Operand
-number = Const . read <$> many1 digit
+number = Immed . read <$> many1 digit
 
--- Parses a single instruction.
-instruction :: Parser Instruction
-instruction = read <$> choice [string "MOV", string "ADD"]
-
+-- Parses an operand. Either a register or an immediate value.
 operand :: Parser Operand
 operand = choice [mode0, mode1, number]
 
+-- Parses a single instruction.
+operator :: Parser Operator
+operator = read <$> choice [string "MOV", string "ADD"]
+
 -- Parses a complete two-operand instruction.
 operation2 :: Parser Operation
-operation2 = TwoOp <$> lexeme instruction <*> lexeme operand <*> lexeme operand
+operation2 = TwoOp <$> lexeme operator <*> lexeme operand <*> lexeme operand
+
+-- Parses any operation (0, 1, or two operands)
+operation :: Parser Operation
+operation = choice [operation2]
+
+-- Parses a label.
+label :: Parser Label
+label = do
+  l <- lexeme $ many1 upper
+  void $ lexeme $ char ':'
+  return l
+
+unlabeledOperation :: Parser Instruction
+unlabeledOperation = Op <$> operation
+
+labeledOperation :: Parser Instruction
+labeledOperation = LOp <$> (Labeled <$> label <*> operation)
+
+-- Parses any operation.
+-- Labeled operations, or none-labeled.
+instruction :: Parser Instruction
+instruction = unlabeledOperation <|> labeledOperation
+
 
 program :: Parser Program
-program = many1 operation2
+program = many1 instruction <* eof
