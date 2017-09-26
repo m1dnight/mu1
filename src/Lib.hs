89493755a5c84b2b8651f97071c75babb9a3aefa
Christophe De Troyer <christophe@call-cc.be>
2017-09-26 16:28:28 +0200

    output to file

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,27 +6,32 @@ import Parser
 import System.Environment
 import Text.Parsec
 import Text.Parsec.String
+import Data.ByteString.Lazy as BS hiding (head, tail, map, foldl)
 
+import Data.Word
+import Data.ByteString.Builder
 
-runAssembler :: IO ()
-runAssembler = do
-  p <- parseFile
-  let (b, _m) = assemble p
-  let bs = printBinaries b
-  putStrLn "AST\n"
-  print p
-  putStrLn "Program binary\n"
-  putStrLn bs
+asm :: IO ()
+asm =  do
+  -- Determine the file to compile
+  args <- getArgs
+  let file = head args
+  let output = head . tail $ args
+  -- Parse the file into an AST
+  ast <- parseFile file
+  mapM_ print ast
+  let (bs, mem) = assemble ast
+  writeToFile output bs
   return ()
 
+writeToFile :: String -> Binaries -> IO ()
+writeToFile f bs = BS.writeFile f binaries
+  where
+    binaries = foldl (\acc b -> append acc (toLazyByteString . word16BE $ b))  empty bs
 
-
-parseFile :: IO Program
-parseFile = do
-  a <- getArgs
-  case a of
-    [as] -> do res <- parseFromFile program as
-               case res of
-                 Left e  -> error $ show e
-                 Right p -> return p
-    _    -> error "Expected exactly one argument"
+parseFile :: String -> IO Program
+parseFile f = do
+  res <- parseFromFile program f
+  case res of
+    Left e  -> error $ show e
+    Right p -> return p
